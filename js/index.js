373e3baf12ed2a2b867a184a6528e14886714217
zxforganic <zxforganic@gmail.com>
2017-08-08 20:20:22 +0900

    第一页到第二页
    第一页基本完了，第二页还只有背景

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var i = self.setInterval("show()", 30);
+var num = 0;
+function show(){
+	document.getElementById("number").innerHTML = num + "%";
+	num++;
+
+	if(num == 101){
+		window.clearInterval(i);
+		window.location.href = "../html/two.html";
+	}
+}
